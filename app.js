991a3d91db500da248edc8f09383930a5ef756b6
jessieguerin <jessie.guerin@cqumail.com>
2025-04-11 20:24:11 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello 12082551);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello 12082551');
+});
+
+app.listen(port, () => {
+    console.log('App running at http://localhost:${port}');
+});
